07b6e0ff4561c8c3a10f06e353b9168e2387ee2c
정은성 <dev.eunseong@gmail.com>
2025-09-01 19:48:28 +0900

    Revert "🔥 :: RTT 패킷 삭제"
    This reverts commit ea71e6cbd2828f3f1b86721a8ba730eb6c04cbba.

--- Protocol/Net_Protocol.h
+++ Protocol/Net_Protocol.h
@@ -1,4 +1,4 @@
-/*class S2C_RES_RTT_PACKET : public iProtocol
+class S2C_RES_RTT_PACKET : public iProtocol
 {
 public:
     S2C_RES_RTT_PACKET() : iProtocol(packet_number::RTT_PACKET) { }
@@ -9,4 +9,4 @@ public:
     long long start_tick;
 
     DEFINE_SERIALIZER_WITH_PARENT(iProtocol, rtt_protocol_number, start_tick)
-};*/
+};
